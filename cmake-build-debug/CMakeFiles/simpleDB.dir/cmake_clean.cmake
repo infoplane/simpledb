5f89ef6681be54a06eea6825305d624828b98b58
Marcus Windle <marcus.windle@info-plane.com>
2019-04-18 13:23:59 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/simpleDB.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpleDB.dir/main.c.obj"
+  "simpleDB.pdb"
+  "simpleDB.exe"
+  "simpleDB.exe.manifest"
+  "simpleDB.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simpleDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
